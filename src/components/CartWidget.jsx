4d51bbac8ee74533a660637b0d1f944bd0aedbe1
Luciana Pireni <lucianapireni@gmail.com>
2025-09-09 12:09:52 -0300

    Entrega 1

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -2,7 +2,7 @@ import "./CartWidget.css"
 export default function CartWidget() {
     return (
         <div className="cart-widget">
-            <a href="https://www.google.com"><img className="cart-img" src="./src/assets/cart-img.png" alt="cart-img"></img></a>
+            <a href="https://www.google.com"><img className="cart-img" src="/imgs/cart-img.png" alt="cart-img"></img></a>
             <span> (0) </span>
         </div>
     );
